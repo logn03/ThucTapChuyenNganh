4c0e0f205a05a9b4666ed22ebe9d42dac69bf2fa
logn03 <plong8838@gmail.com>
2025-12-02 08:05:34 +0700

    trang gio hang, trang chi tiet san pham

--- javascript/MainJs.js
+++ javascript/MainJs.js
@@ -1,5 +1,10 @@
 import { suKien } from './SuKien.js';
+import './JSTatCaSanPham.js';
+import './ChiTietSanPham.js';
 
 document.addEventListener("DOMContentLoaded", () => {
   suKien();
+      // Khởi tạo logic từ cả hai file
+    initializeSidebar();
+    initializeProductDetails();
 });
